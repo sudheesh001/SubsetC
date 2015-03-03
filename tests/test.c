d31f4103230575e40e46387519e7b1a540cc97af
sudheesh001 <sudheesh1995@outlook.com>
2015-03-03 16:10:12 +0530

    Adding enhanced test file which has to pass

--- tests/test.c
+++ tests/test.c
@@ -1,6 +1,26 @@
 int main(int argc, char **argv) {
-	int a,b;
-	a = 5;
-	b = 6;
+	
+	int arr[] = {10, 15, 25};
+	int assignedVar = 10;
+
+	int var = arr[0]+arr[1]+arr[2];
+
+	int a = 10;
+	int b = 15;
+
+	while ( a != 0 && b > 0) {
+		if (a >= b) {
+			a /= b;
+			b *= a;
+		}
+		else
+		if (b == 0 || a < b) {
+			b /= a;
+			a = 0;
+		}
+		a--;
+		b++;
+	}
+
 	return 0;
 }
